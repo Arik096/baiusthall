50a9171140c7954e28d9dbafa24ba95a44981a94
Arik Md. Isthiaque <arikbncc@gmail.com>
2019-05-02 21:42:27 +0600

    Create edit_post_core.php

---
+++ edit_post_core.php
@@ -0,0 +1,27 @@
+<?php
+include("lib/session.php");
+  if(isset($_REQUEST['update_btn'])){
+    if(empty($_REQUEST['title']) || empty($_REQUEST['author']) || empty($_REQUEST['description'])){
+     $msg="Field must not be Empty";
+    }
+    else{
+      $title=$_REQUEST['title'];
+      $author= $_REQUEST['author'];
+      $description=$_REQUEST['description'];
+      $id=$_REQUEST['id'];
+
+      spl_autoload_register(function($class){
+          include("class/".$class.".php");
+        });
+        $post=new PostNotice();
+        $result=$post->update_notice($title,$author,$description,$id);
+        if($result){
+          header("location:view_post_management.php");
+        }
+        else {
+         $msg="Not Update";
+        }
+
+    }
+  }
+ ?>
