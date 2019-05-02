cc6e7dc2f18e9c93c80e5f793f748479bd12c837
Arik Md. Isthiaque <arikbncc@gmail.com>
2019-05-03 00:20:35 +0600

    Update management.php

--- management.php
+++ management.php
@@ -1,8 +1,7 @@
-<?php include("session.php"); ?>
+<?php include("lib/session.php"); ?>
 
    <?php include("management_header.php"); ?>
    <br><br><br>
     <center><h1>Wellcome to management page</h1></center>
    <br><br><br>
    <?php include("footer.php"); ?>
-   <?php include("cdn.php"); ?>
