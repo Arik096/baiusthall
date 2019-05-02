3f5e8f82ddff5007207829d26ac7ea7a6d5e49b0
Arik Md. Isthiaque <arikbncc@gmail.com>
2019-05-03 00:21:01 +0600

    Create provost.php

---
+++ provost.php
@@ -0,0 +1,40 @@
+<?php include("management_header.php"); ?>
+<?php
+  include("lib/session.php");
+  include("lib/mini_conection.php");
+  spl_autoload_register(function($class){
+      include("class/".$class.".php");
+    });
+  $check=new CheckTable();
+  $rows=$check->check_data("tbl_provost");
+  if($rows<1){
+    ?>
+      <a href="add_provost.php" class="btn btn-success mt-3">Add Provost</a>
+    <?php
+  }else{
+    $query="select * from tbl_provost";
+    $result=mysqli_query($connect,$query);
+    $data=mysqli_fetch_assoc($result);
+        ?>
+        <div class="mt-4">
+          <div class="row">
+            <div class="col-lg-12">
+              <center class="mt-4"><h2><span class="mb-5" style="border:10px solid black; padding:5px; color:#F59B29;">Provost</span></h2><center>
+              <center><img width="20%" style="border-radius:100%;" class="mt-5"  src="management_picture/<?php echo $data['pic'];?>" alt=""></center>
+              <div class="text-center">
+                <h2 class="text-dark"><span><?php echo $data['name'];?></span></h2>
+                <p><?php echo $data['description']; ?></p>
+                <p><?php echo $data['phn_num']; ?></p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <?php
+
+    }
+
+
+
+ ?>
+
+<?php include("footer.php"); ?>
