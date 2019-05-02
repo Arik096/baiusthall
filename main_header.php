712c81ee658454d94378be48debb7d69e0a0fd6d
Arik Md. Isthiaque <arikbncc@gmail.com>
2019-05-03 00:20:31 +0600

    Update main_header.php

--- main_header.php
+++ main_header.php
@@ -36,7 +36,7 @@
             <a class="nav-link" href="index.php">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Provost</a>
+            <a class="nav-link" href="view_provost.php">Provost</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Assistant Provost</a>
