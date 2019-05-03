bde4e4b84f017c7bbdca01fa88673b77d81e955b
Arik Md. Isthiaque <arikbncc@gmail.com>
2019-05-04 01:56:51 +0600

    Update volanteers.php

--- volanteers.php
+++ volanteers.php
@@ -1,8 +1,46 @@
 <?php include("management_header.php"); ?>
 <?php
   include("lib/session.php");
+  include("lib/mini_conection.php");
+  ?>
+    <a href="add_volanteer.php" class="btn btn-success mt-3">Add Volanteer</a>
+  <?php
+    $query="select * from tbl_hr";
+    $result=mysqli_query($connect,$query);
+    ?>
+      <center class="mt-4"><h2><span class="mb-5" style="border:10px solid black; padding:5px; color:#F59B29;">Volanteers</span></h2><center>
+        <div class="container mt-4">
+          <table class="table table-hover">
+            <thead class="thead-light">
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Possition</th>
+                <th scope="col">Description</th>
+                <th scope="col">Phone Number</th>
+                <th scope="col">Picture</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
 
+            <?php
+            while($data=mysqli_fetch_assoc($result)) {
 
- ?>
+              ?>
+              <tbody>
+                <tr>
+                  <td><?php echo $data['name']; ?></td>
+                  <td><?php echo $data['position']; ?></td>
+                  <td><?php echo $data['description']; ?></td>
+                  <td><?php echo $data['phn_num']; ?></td>
+                  <td><img width="50" height="50" src="management_picture/<?php echo $data['pic'];?>" alt=""></td>
+                  <td><a class="btn btn-primary" href="edit_volanteer.php?id=<?php echo $data['id']?>">Edit</a><a onclick='return confirm("Are you sure")' class="btn btn-primary ml-2" href="delete_volanteer.php?id=<?php echo $data['id']?>">Delete</a></td>
+                </tr>
+              </tbody>
+              <?php
+            }
+         ?>
+
+          </table>
+        </div>
 
 <?php include("footer.php"); ?>
