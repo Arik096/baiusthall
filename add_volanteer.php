9a13a7598ceb0ce5e6a8c89ab696b74148333ec5
Arik Md. Isthiaque <arikbncc@gmail.com>
2019-05-04 01:55:24 +0600

    Create add_volanteer.php

---
+++ add_volanteer.php
@@ -0,0 +1,42 @@
+<?php include("session.php"); ?>
+
+
+   <?php include("management_header.php"); ?>
+   <?php
+      include("add_volanteer_core.php");
+    ?>
+   <!--form-->
+   <div class="container mt-3">
+     <form action="" method="post" class="post-form-custom" enctype="multipart/form-data">
+     <div class="form-group">
+       <label for="exampleInputEmail1">Name</label>
+       <input type="text" class="form-control" name="name">
+     </div>
+     <div class="form-group">
+       <label for="exampleInputEmail1">Postion</label>
+       <input type="text" class="form-control" name="position">
+     </div>
+     <div class="form-group">
+       <label for="comment">Description:</label>
+       <textarea class="form-control" rows="5" id="comment" name="description"></textarea>
+     </div>
+     <div class="form-group">
+       <label for="exampleInputEmail1">Phone Number</label>
+       <input type="text" class="form-control" name="phone_number">
+     </div>
+     <div class="form-group">
+       <label for="exampleInputEmail1">Photo</label>
+       <input type="file" name="file">
+     </div>
+
+     <button type="submit" class="btn btn-primary" name="post_btn">Add Volanteer</button>
+     </form>
+     <?php
+      if(!empty($errorMsg)){
+        echo $errorMsg;
+      }
+      ?>
+   </div>
+
+
+   <?php include("footer.php"); ?>
