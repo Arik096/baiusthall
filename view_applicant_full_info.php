8936c3ad9df8ff9810435e084c23adce7183b123
Arik Md. Isthiaque <arikbncc@gmail.com>
2019-05-11 06:11:57 +0600

    Create view_applicant_full_info.php

---
+++ view_applicant_full_info.php
@@ -0,0 +1,135 @@
+<?php include("management_header.php"); ?>
+<?php
+  include("lib/session.php");
+  include("lib/mini_conection.php");
+  ?>
+  <?php
+    if(isset($_REQUEST['id'])){
+      $id=$_REQUEST['id'];
+      $query="SELECT * FROM tbl_new_std_info NATURAL JOIN tbl_new_std_pa NATURAL JOIN tbl_new_std_ca NATURAL JOIN tbl_new_std_guardian_info WHERE std_id='$id'";
+      if($result=mysqli_query($connect,$query)){
+        $data=mysqli_fetch_assoc($result);
+      }
+    }
+
+  ?>
+  <div class="container mt-2">
+    <center><div class="mb-4">
+      <img src="<?php echo $data['picture'];?>" height="200" width="200">
+    </div></center>
+    <div class="row">
+      <div class="col-lg-4">
+        <div class="mb-2">
+          <span><b>1.Personal Info:</b></span>
+        </div>
+        <div class="">
+          <label>Student Name: <?php echo " ".$data['std_name'];?></label>
+        </div>
+        <div class="">
+          <label>Student ID: <?php echo " ".$data['std_id'];?></label>
+        </div>
+        <div class="">
+          <label>Session: <?php echo " ".$data['std_session'];?></label>
+        </div>
+        <div class="">
+          <label>Department: <?php echo " ".$data['std_dept'];?></label>
+        </div>
+        <div class="">
+          <label>Level: <?php echo " ".$data['std_level'];?></label>
+        </div>
+        <div class="">
+          <label>Term: <?php echo " ".$data['std_term'];?></label>
+        </div>
+        <div class="">
+          <label>Gender: <?php echo " ".$data['std_gender'];?></label>
+        </div>
+        <div class="">
+          <label>Phone Number: <?php echo " ".$data['std_phone'];?></label>
+        </div>
+        <div class="">
+          <label>Email: <?php echo " ".$data['std_email'];?></label>
+        </div>
+        <div class="">
+          <label>Religion: <?php echo " ".$data['std_religion'];?></label>
+        </div>
+        <div class="">
+          <label>Nationality: <?php echo " ".$data['std_nationality'];?></label>
+        </div>
+        <div class="">
+          <label>Date of Birth: <?php echo " ".$data['std_dob'];?></label>
+        </div>
+      </div>
+
+      <div class="col-lg-4">
+        <div class="mb-2">
+          <span><b>2.Parmanent Address:</b></span>
+        </div>
+        <div class="">
+          <label>Division: <?php echo " ".$data['pa_division'];?></label>
+        </div>
+        <div class="">
+          <label>District: <?php echo " ".$data['pa_district'];?></label>
+        </div>
+        <div class="">
+          <label>P.S/Upazilla: <?php echo " ".$data['pa_upazilla'];?></label>
+        </div>
+        <div class="">
+          <label>Post Office/Code: <?php echo " ".$data['pa_po'];?></label>
+        </div>
+        <div class="">
+          <label>Village/House & Road: <?php echo " ".$data['pa_village'];?></label>
+        </div>
+
+        <div class="mt-3 mb-2">
+          <span><b>3.Present Address:</b></span>
+        </div>
+        <div class="">
+          <label>Division: <?php echo " ".$data['ca_division'];?></label>
+        </div>
+        <div class="">
+          <label>District: <?php echo " ".$data['ca_district'];?></label>
+        </div>
+        <div class="">
+          <label>P.S/Upazilla: <?php echo " ".$data['ca_upazilla'];?></label>
+        </div>
+        <div class="">
+          <label>Post Office/Code: <?php echo " ".$data['ca_po'];?></label>
+        </div>
+        <div class="">
+          <label>Village/House & Road: <?php echo " ".$data['ca_village'];?></label>
+        </div>
+
+      </div>
+
+
+
+      <div class="col-lg-4">
+        <div class="mb-2">
+          <span><b>4.Guardian Info:</b></span>
+        </div>
+        <div class="">
+          <label>Father's Name: <?php echo " ".$data['father_name'];?></label>
+        </div>
+        <div class="">
+          <label>Mother's Name: <?php echo " ".$data['mother_name'];?></label>
+        </div>
+        <div class="">
+          <label>Contact Number: <?php echo " ".$data['contact_no'];?></label>
+        </div>
+        <div class="">
+          <label>Guardian Name: <?php echo " ".$data['guardian_name'];?></label>
+        </div>
+        <div class="">
+          <label>Relation: <?php echo " ".$data['relation'];?></label>
+        </div>
+        <div class="">
+          <label>Contact Number: <?php echo " ".$data['guardian_phone'];?></label>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+  <?php include("footer.php"); ?>
