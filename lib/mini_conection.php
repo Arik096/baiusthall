8ea1b4ec830198224715160eec0a97480802faab
Arik Md. Isthiaque <arikbncc@gmail.com>
2019-05-18 04:51:53 +0600

    Update mini_conection.php

--- lib/mini_conection.php
+++ lib/mini_conection.php
@@ -1,4 +1,8 @@
 <?php
+//sql211.epizy.com
+//epiz_23793784_baiust_hall
+//epiz_23793784
+//bVVS9kLvr
 $connect=mysqli_connect("localhost","root","");
 if($connect){
   $dbselect=mysqli_select_db($connect,"baiust_hall");
