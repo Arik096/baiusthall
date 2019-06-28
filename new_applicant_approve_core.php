1e2d008717f6ececab4d369a163b2506ea561003
Arik Md. Isthiaque <arikbncc@gmail.com>
2019-06-29 01:12:02 +0600

    Update new_applicant_approve_core.php

--- new_applicant_approve_core.php
+++ new_applicant_approve_core.php
@@ -1,5 +1,5 @@
 <?php
-    /*include("lib/mini_conection.php");
+    include("lib/mini_conection.php");
     if(isset($_REQUEST['btn_allot'])){
       $id=$_REQUEST['std_id'];
       $query="SELECT * FROM tbl_new_std_info NATURAL JOIN tbl_new_std_pa NATURAL JOIN tbl_new_std_ca NATURAL JOIN tbl_new_std_guardian_info WHERE std_id='$id'";
@@ -34,47 +34,49 @@
       $z=$data['guardian_name'];
       $a1=$data['relation'];
       $a2=$data['guardian_phone'];
-      $b1=$target_file;
+      $b1=$data['picture'];
 
-      $sql_one="INSERT INTO tbl_new_std_info(std_name,std_id,std_session,std_dept,std_level,std_term,std_gender,std_phone,std_email,std_religion,std_nationality,std_dob) VALUES('$a','$b','$c','$d','$e','$f','$g','$h','$i','$j','$k','$l')";
-      $sql_two="INSERT INTO tbl_new_std_pa(std_id,pa_division,pa_district,pa_upazilla,pa_po,pa_village) VALUES('$b','$m','$n','$o','$p','$q')";
-      $sql_three="INSERT INTO tbl_new_std_ca(std_id,ca_division,ca_district,ca_upazilla,ca_po,ca_village) VALUES('$b','$r','$s','$t','$u','$v')";
-      $sql_four="INSERT INTO tbl_new_std_guardian_info(std_id,father_name,mother_name,contact_no,guardian_name,relation,guardian_phone,picture) VALUES('$b','$w','$x','$y','$z','$a1','$a2','$b1')";
+
+
+      $sql_one="INSERT INTO tbl_std_info(std_name,std_id,std_session,std_dept,std_level,std_term,std_gender,std_phone,std_email,std_religion,std_nationality,std_dob) VALUES('$a','$b','$c','$d','$e','$f','$g','$h','$i','$j','$k','$l')";
+      $sql_two="INSERT INTO tbl_std_pa(std_id,pa_division,pa_district,pa_upazilla,pa_po,pa_village) VALUES('$b','$m','$n','$o','$p','$q')";
+      $sql_three="INSERT INTO tbl_std_ca(std_id,ca_division,ca_district,ca_upazilla,ca_po,ca_village) VALUES('$b','$r','$s','$t','$u','$v')";
+      $sql_four="INSERT INTO tbl_std_guardian_info(std_id,father_name,mother_name,contact_no,guardian_name,relation,guardian_phone,picture) VALUES('$b','$w','$x','$y','$z','$a1','$a2','$b1')";
       $query_one=mysqli_query($connect,$sql_one);
       $query_two=mysqli_query($connect,$sql_two);
       $query_three=mysqli_query($connect,$sql_three);
       $query_four=mysqli_query($connect,$sql_four);
-      if($query_one && $query_two && $query_three && $query_four)
+      if($query_one && $query_two && $query_three && $query_four){
+            $table = $_REQUEST['hall_select'];
+            $std_id=$_REQUEST['std_id'];
+            $floor=$_REQUEST['floor_no'];
+            $flat=$_REQUEST['flat_no'];
+            $room=$_REQUEST['room_no'];
+            $bed=$_REQUEST['bed_no'];
 
-
-
-
-
-      //var_dump($data);
-      $table = $_REQUEST['hall_select'];
-      $std_id=$_REQUEST['std_id'];
-      $floor=$_REQUEST['floor_no'];
-      $flat=$_REQUEST['flat_no'];
-      $room=$_REQUEST['room_no'];
-      $bed=$_REQUEST['bed_no'];
-
-      if($table=='1'){
-        $sql="UPDATE tbl_male_hall_1 SET std_id='$std_id' WHERE floor_no='$floor' AND  flat_no='$flat' AND room_no='$room' AND bed_no='$bed'";
-        $result=mysqli_query($connect,$sql);
+            if($table=='1'){
+              $sql="UPDATE tbl_male_hall_1 SET std_id='$std_id' WHERE floor_no='$floor' AND  flat_no='$flat' AND room_no='$room' AND bed_no='$bed'";
+              $result=mysqli_query($connect,$sql);
+            }
+            elseif ($table=='2') {
+              $sql="UPDATE tbl_male_hall_1 SET std_id='$std_id'";
+              $result=mysqli_query($connect,$sql);
+            }
+            elseif ($table=='17') {
+              $sql="UPDATE tbl_17_c SET std_id='$std_id' WHERE floor_no='$floor' AND room_no='$room' AND bed_no='$bed'";
+              $result=mysqli_query($connect,$sql);
+            }
+            if($result){
+              $delete_data="DELETE FROM tbl_new_std_info WHERE std_id='$b'";
+              mysqli_query($connect,$delete_data);
+              rename("application_picture/$b1", "student_picture/$b1");
+              unlink("application_picture/$b1");
+              header("location: new_std_application.php?msg=Alloted Success");
+            }
       }
-      elseif ($table=='2') {
-        $sql="UPDATE tbl_male_hall_1 SET std_id='$std_id'";
-        $result=mysqli_query($connect,$sql);
-      }
-      elseif ($table=='17') {
-        $sql="UPDATE tbl_male_hall_1 SET std_id='$std_id' WHERE floor_no='$floor' AND room_no='$room' AND bed_no='$bed'";
-        $result=mysqli_query($connect,$sql);
-      }
-      if($result){
-        header("location: new_std_application.php.php?msg=Alloted Success");
+      else{
+        die(mysqli_error($connect));
       }
 
-
-
-    }*/
+    }
  ?>
