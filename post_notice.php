86d843d0568cffd820041d8fb2cfd0d81ab80336
Arik Md. Isthiaque <arikbncc@gmail.com>
2019-06-28 21:57:00 +0600

    Update post_notice.php

--- post_notice.php
+++ post_notice.php
@@ -6,7 +6,7 @@
  ?>
 <!--form-->
 <div class="container mt-3">
-  <form action="post_notice_core.php" method="post" class="post-form-custom">
+  <form action="post_notice_core.php" method="post" class="post-form-custom" enctype="multipart/form-data">
   <div class="form-group">
     <label for="exampleInputEmail1">Title</label>
     <input type="text" class="form-control" name="title">
@@ -24,10 +24,10 @@
     <label for="comment">Notice Description:</label>
     <textarea class="form-control" rows="5" id="comment" name="description"></textarea>
   </div>
-  <!--<div class="form-group">
+  <div class="form-group">
     <label for="file">Upload Picture:</label>
     <input type="file" name="notice_pic" value="">
-  </div>-->
+  </div>
   <button type="submit" class="btn btn-primary" name="post_btn">Post</button>
   </form>
   <div>
