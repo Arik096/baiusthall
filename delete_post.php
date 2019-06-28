481d6bd652a5fec4397a0ad9127201672b69df49
Arik Md. Isthiaque <arikbncc@gmail.com>
2019-06-28 21:56:27 +0600

    Update delete_post.php

--- delete_post.php
+++ delete_post.php
@@ -1,14 +1,21 @@
 <?php
 include("lib/session.php");
+include("lib/mini_conection.php");
 
       $id=$_REQUEST['id'];
 
       spl_autoload_register(function($class){
           include("class/".$class.".php");
         });
+        $sql="SELECT pic FROM notice_board WHERE id=$id";
+        $result=mysqli_query($connect,$sql);
+        $data=mysqli_fetch_assoc($result);
+
         $post=new PostNotice();
         $result=$post->delete_notice($id);
         if($result){
+
+          unlink($data['pic']);
           header("location:view_post_management.php");
         }
  ?>
