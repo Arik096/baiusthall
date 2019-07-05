0d26bff4874c0ae043cc94c7ce0592001ee6ac3f
Arik Md. Isthiaque <arikbncc@gmail.com>
2019-07-06 02:34:53 +0600

    Create 17_c_hall_single_std_info.php

---
+++ 17_c_hall_single_std_info.php
@@ -0,0 +1,22 @@
+<div class="w-80">
+      <div class="container mt-4">
+        <table class="table table-hover">
+          <thead class="thead-light">
+            <tr>
+              <th scope="col">Building No</th>
+              <th scope="col">Room No</th>
+              <th scope="col">Bed No</th>
+            </tr>
+          </thead>
+            <tbody>
+              <tr>
+                <td><?php echo $data2['building_no']; ?></td>
+                <td><?php echo $data2['room_no']; ?></td>
+                <td><?php echo $data2['bed_no']; ?></td>
+              </tr>
+            </tbody>
+        </table>
+
+      </div>
+
+</div>
