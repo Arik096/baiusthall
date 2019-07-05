13e05bc6db98cdd6f407af8612e7632ccce145d3
Arik Md. Isthiaque <arikbncc@gmail.com>
2019-07-06 02:35:14 +0600

    Create male_hall_1_single_std_info.php

---
+++ male_hall_1_single_std_info.php
@@ -0,0 +1,24 @@
+<div class="w-80">
+      <div class="container mt-4">
+        <table class="table table-hover">
+          <thead class="thead-light">
+            <tr>
+              <th scope="col">Floor No</th>
+              <th scope="col">Flat No</th>
+              <th scope="col">Room No</th>
+              <th scope="col">Bed No</th>
+            </tr>
+          </thead>
+            <tbody>
+              <tr>
+                <td><?php echo $data1['floor_no']; ?></td>
+                <td><?php echo $data1['flat_no']; ?></td>
+                <td><?php echo $data1['room_no']; ?></td>
+                <td><?php echo $data1['bed_no']; ?></td>
+              </tr>
+            </tbody>
+        </table>
+
+      </div>
+
+</div>
