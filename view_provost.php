96f23d362f895c5801684e938ce0b8131a889244
Arik Md. Isthiaque <arikbncc@gmail.com>
2019-07-06 23:52:18 +0600

    Update view_provost.php

--- view_provost.php
+++ view_provost.php
@@ -9,7 +9,7 @@
           <div class="row">
             <div class="col-lg-12">
               <center class="mt-4"><h2><span class="mb-5" style="border:10px solid black; padding:5px; color:#F59B29;">Provost</span></h2><center>
-              <center><img width="20%" style="border-radius:100%;" class="mt-5"  src="management_picture/<?php echo $data['pic'];?>" alt=""></center>
+              <center><img width="20%" style="border-radius:100%;" class="mt-5"  src="<?php echo $data['pic'];?>" alt=""></center>
               <div class="text-center">
                 <h2 class="text-dark"><span><?php echo $data['name'];?></span></h2>
                 <h3><?php echo $data['position']; ?></h3>
