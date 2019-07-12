f28aeb1e9a79cd190fd1d4a77b6d64220246cb19
Arik Md. Isthiaque <arikbncc@gmail.com>
2019-07-13 01:13:54 +0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -48,6 +48,7 @@
   <div class="mb-4"></div>
 </div>
 <script type="text/javascript" src="js/main.js"></script>
+<script type="text/javascript" src="js/std.js"></script>
 
 <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
