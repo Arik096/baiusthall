bd11284ac2f7908365dc5a0a4f55e1be2a97f0a4
Arik Md. Isthiaque <arikbncc@gmail.com>
2019-07-13 01:14:28 +0600

    Create std_login.php

---
+++ std_login.php
@@ -0,0 +1,7 @@
+<?php include('std_header.php') ?>
+  <div class="container mt-5">
+    <h1 class="text-center">Welcome to <b><?php echo $row['std_name'] ?></b></h1>
+  </div>
+  <?php include('footer.php'); ?>
+</body>
+</html>
